9e555b0ffe10dc90e0883770cbad8783050c0462
mohammad hossein <mh.abolhasanii@gmail.com>
2026-08-22 12:18:40 +0330

    Revert "add log to service"
    This reverts commit 046eb8c852ecdef3769dab020f3d60e46b24b319.

--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -2,7 +2,6 @@ package com.example.library.service;
 
 import com.example.library.dto.Book;
 import com.example.library.exception.ValidationException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -12,7 +11,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
-@Slf4j
 public class BookService {
 
     private final Map<Long, Book> books = new ConcurrentHashMap<>();
@@ -23,55 +21,36 @@ public class BookService {
     private static final String NOT_FOUND_ERROR_MESSAGE = "book not found";
 
     public Book add(Book book) {
-        log.info("add new book");
-
         long id = ID_SEQUENCE.getAndIncrement();
         book.setId(id);
         books.put(id, book);
-
-        log.info("book added successfully . id: {}", id);
-
         return book;
     }
 
     public List<Book> getAll() {
-        log.info("getting all books");
-
         return books.values().stream()
                 .sorted(Comparator.comparingLong(Book::getId))
                 .toList();
     }
 
     public Book get(long id) {
-        log.info("getting book with id: {}", id);
-
         if(!books.containsKey(id)){
-            log.warn("book not found with id: {}", id);
             throw new ValidationException(NOT_FOUND_ERROR_CODE, NOT_FOUND_ERROR_MESSAGE);
         }
         return books.get(id);
     }
 
     public Book update(long id, Book book) {
-        log.info("updating book with id: {}", id);
-
         if (!books.containsKey(id)) {
-            log.warn("book not found with id: {}", id);
             throw new ValidationException(NOT_FOUND_ERROR_CODE , NOT_FOUND_ERROR_MESSAGE);
         }
         book.setId(id);
         books.put(id, book);
-
-        log.info("Book updated successfully with id: {}", id);
-
         return book;
     }
 
     public Book patch(long id, Book book) {
-        log.info("patch book with id: {}", id);
-
         if(!books.containsKey(id)){
-            log.warn("book not found with id: {}", id);
             throw new ValidationException(NOT_FOUND_ERROR_CODE , NOT_FOUND_ERROR_MESSAGE);
         }
         Book savedBook = books.get(id);
@@ -81,21 +60,13 @@ public class BookService {
         if (book.getName() != null) {
             savedBook.setName(book.getName());
         }
-
-        log.info("book patched successfully with id: {}", id);
-
         return savedBook;
     }
 
     public void delete(long id) {
-        log.info("delete book with id: {}", id);
-
         if(!books.containsKey(id)){
-            log.warn("book not found with id: {}", id);
             throw new ValidationException(NOT_FOUND_ERROR_CODE, NOT_FOUND_ERROR_MESSAGE);
         }
         books.remove(id);
-
-        log.info("book deleted successfully with id: {}", id);
     }
 }
